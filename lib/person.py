c42ba6d0ae70395e69aa704898e7a087a5b5099d
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-02-16 00:47:01 +0300

    Completed lab

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,13 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+Sally = Person()
+Sally.talk()
+Sally.walk()
